3abe2d35848abd81aa54c4a0e7b42db7e1797cf9
TimCock <83985212+TimCock@users.noreply.github.com>
2024-09-05 14:20:12 +0300

    Update install.sh

--- deploy/selfhost/install.sh
+++ deploy/selfhost/install.sh
@@ -183,7 +183,7 @@ function buildYourOwnImage(){
     local PLANE_TEMP_CODE_DIR=~/tmp/plane
     rm -rf $PLANE_TEMP_CODE_DIR
     mkdir -p $PLANE_TEMP_CODE_DIR
-    REPO=https://github.com/TimCock/plane-radar.git
+    REPO=https://github.com/TimC99ock/plane-radar.git
     git clone "$REPO" "$PLANE_TEMP_CODE_DIR"  --branch "$BRANCH" --single-branch --depth 1
 
     cp "$PLANE_TEMP_CODE_DIR/deploy/selfhost/build.yml" "$PLANE_TEMP_CODE_DIR/build.yml"
